66e9af9a44b512d32f1059b4960a480ede087441
KwonDongSung <61718726+KwonDongSung@users.noreply.github.com>
2020-05-19 22:12:22 +0900

    Update calc.c

--- calc.c
+++ calc.c
@@ -4,17 +4,22 @@
 
 int main(){
 string calc[100];
- string num[100];
- string oper[100];
+string num[100];
+string oper[100];
+int k = 0;
+int j = 0;
 printf ("수식을 입력하세요 : ");
 scanf ("%s",calc);
-for(int i=0; i< strlen(calc); i++);
-  int k = 0;
-  if( is_operater(calc[1]) == true){
-    oper[k] = calc[1];
+for(int i=0; i< strlen(calc); i++){
+  if( is_operater(calc[i]) == true){
+    oper[k] = calc[i];
     k++;
+    }
+  else
+    num[j] = calc[i];
 }
-}
+ 
+printf("결과는 : %d ", 
 }
 
 bool is_operator(string a){
