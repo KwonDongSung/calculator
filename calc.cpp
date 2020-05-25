1624653a69ca19b5f6cf8fccd54ea9c4b9bdcdee
KwonDongSung <61718726+KwonDongSung@users.noreply.github.com>
2020-05-26 00:27:51 +0900

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,19 +1,31 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+using namespace std;
+#include <string>
 
-bool is_operator(string a);
+bool is_operator(char a){
+if ((a == '+') || (a == '-') || (a == '*') || (a == '/'))
+  return true;
+  else return false;
+}
+
+int test(int a, string b, int c){
+  if (b == "+") return a + c;
+  if (b == "-") return a - c;
+  if (b == "*") return a * c;
+  if (b == "/") return a / c;
+}
 
 int main(){
-string calc[100];
-string num[100];
-string oper[100];
+char calc[100];
+int num[100];
+char oper[100];
 int ans = 0;
 int k = 0;
 int j = 0;
 printf ("수식을 입력하세요 : ");
 scanf ("%s",calc);
-for(int i=0; i< strlen(calc); i++){
-  if( is_operater(calc[i]) == true){
+for(int i=0; i< 10; i++){
+  if( is_operator(calc[i]) == true){
     oper[k] = calc[i];
     k++;
     }
@@ -24,22 +36,11 @@ for(int i=0; i< strlen(calc); i++){
 }
 int m = 0;
 int sth = num[0];
-for(int n = 1; n < strlen(calc); n++){
-  sth = test(sth ,oper[m],int num[n]);
+for(int n = 1; n < 7; n++){
+  if (num[n] == 0) break;
+  sth = test(sth ,oper[m],num[n]);
   m++;
 }
-
+  
 printf("결과는 : %d ", ans);
-}
-
-bool is_operator(string a){
-if ((a == +) || (a == -) || (a == *) || (a == /))
-  return true;
-  else return false;
-}
-int test(int a, string b, int c){
-  if (b == "+") return a + c;
-  if (b == "-") return a - c;
-  if (b == "*") return a * c;
-  if (b == "/") return a / c;
 }
